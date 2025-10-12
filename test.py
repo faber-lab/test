54967fc6b2e55252e14a782696db0f603d98221d
faber-lab <faber@maia.eonet.ne.jp>
2025-10-12 13:35:56 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,15 +3,28 @@ from googleapiclient.discovery import build
 import io
 from googleapiclient.http import MediaIoBaseUpload
 
-# --- 設定 ---
-SERVICE_ACCOUNT_FILE = "/etc/secrets/autoyoutube-474207-2bc16b07ab8f.json"  # Render上に置く or 環境変数で読み込む
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    "client_secret_599500399129-nqah0dbjj0ikk8l2r17304gr1l2ombdm.apps.googleusercontent.com.json", SCOPES)
+creds = flow.run_local_server(port=0)
+
+service = build("drive", "v3", credentials=creds)
+
+
+# --- 設定 ---
+#SERVICE_ACCOUNT_FILE = "/etc/secrets/autoyoutube-474207-2bc16b07ab8f.json"  # Render上に置く or 環境変数で読み込む
+#SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 FOLDER_ID = "1BT8_ri8ukTlyahgDpWVHY6QDXH6s7JBy"  # アップロード先のフォルダID
 
 # --- 認証 ---
-creds = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
-)
+#creds = service_account.Credentials.from_service_account_file(
+#    SERVICE_ACCOUNT_FILE, scopes=SCOPES
+#)
 
 # --- Drive API 初期化 ---
 service = build("drive", "v3", credentials=creds)
