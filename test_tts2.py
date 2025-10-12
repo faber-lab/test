63541a1366caee232010369908d2d04cec492944
faber-lab <faber@maia.eonet.ne.jp>
2025-10-13 01:28:11 +0900

    Update test_tts2.py

--- test_tts2.py
+++ test_tts2.py
@@ -1,3 +1,7 @@
+import os
+import google.generativeai as genai
+from google.genai import types
+
 model_list = genai.list_models()
 for m in model_list:
     print(m.name, m.supported_generation_methods)
